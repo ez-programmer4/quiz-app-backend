d9eb378811a08a8d31e01275814eca2ba3261796
ezedin ebrahim <ezedinebrahim131@gmail.com>
2024-12-13 10:12:23 +0300

    bcrypt2

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const userRoute = require("./routes/userRoutes");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; // Use the PORT from the environment or default to 5000
 
 app.use(cors());
 app.use(express.json());
@@ -23,6 +23,8 @@ app.use("/api/categories", categoryRoutes);
 app.use("/api/quizzes", quizRoutes); // Use quiz routes
 app.use("/api/result", resultRoute);
 app.use("/api/progress", progressRoutes);
+
+// Update the listen method for Render
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`); // Log the port instead of a specific URL
 });
